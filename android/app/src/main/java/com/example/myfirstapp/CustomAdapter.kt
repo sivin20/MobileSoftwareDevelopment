79cc5df4103754a6f8f681604e4cc03f35c84afc
sivin20 <sivin20@student.sdu.dk>
2022-10-19 17:11:58 +0200

    adds functionality for fragments upon viewitem click

--- android/app/src/main/java/com/example/myfirstapp/CustomAdapter.kt
+++ android/app/src/main/java/com/example/myfirstapp/CustomAdapter.kt
@@ -3,10 +3,14 @@ package com.example.myfirstapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.myfirstapp.database.AppDatabase
 import com.example.myfirstapp.database.Movie
+import com.example.myfirstapp.fragment.MovieFragment
 
 
 class CustomAdapter(private val dataSet: List<Movie>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
@@ -14,6 +18,7 @@ class CustomAdapter(private val dataSet: List<Movie>) : RecyclerView.Adapter<Cus
         val movieTitle : TextView = view.findViewById(R.id.movieTitle)
         val movieOverview : TextView = view.findViewById(R.id.movieOverview)
         val movieRelease : TextView = view.findViewById(R.id.movieRelease)
+        val deleteRow : ImageView = view.findViewById(R.id.deleteRow)
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
@@ -27,5 +32,13 @@ class CustomAdapter(private val dataSet: List<Movie>) : RecyclerView.Adapter<Cus
         holder.movieTitle.text = dataSet[position].title
         holder.movieOverview.text =dataSet[position].overview
         holder.movieRelease.text =dataSet[position].release_date
+        holder.itemView.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                val activity=v!!.context as AppCompatActivity
+                val movieFragment=MovieFragment()
+                activity.supportFragmentManager.beginTransaction().replace(R.id.constraintLayout, movieFragment).addToBackStack(null).commit()
+            }
+
+        })
     }
 }
